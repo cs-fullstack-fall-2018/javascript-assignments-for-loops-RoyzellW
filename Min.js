d86e2ce4cbadae2422517df6cdeb89fa6eff3328
student <royzellwalton@gmail.com>
2018-08-01 15:50:04 -0500

    Done with finding largest number

---
+++ Min.js
@@ -0,0 +1,10 @@
+
+
+var myNumberList = [12, 2112, 9000, 1999, 24];
+var biggestNum = 0;
+for(var i=0; i< myNumberList.length; i++){
+    if(myNumberList[i] > biggestNum){
+        biggestNum = myNumberList[i];
+    }
+}
+console.log(biggestNum);
